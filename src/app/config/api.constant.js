f49632a406ec19aebde75b5b31357fd86fb52d42
Lucas Teles <lucas@taginterativa.com.br>
2017-01-15 16:36:59 -0200

    api constant name updated to improve readability

---
+++ src/app/config/api.constant.js
@@ -0,0 +1,16 @@
+(function(angular) {
+	'use strict';
+
+	var api	= {};
+
+	api.baseUrl = '/api/';
+
+	api.cars = resourceOf('cars');
+
+	angular.module('app').constant('API', api);
+
+	function resourceOf(resourceName) {
+		return api.baseUrl + resourceName + '.json';
+	}
+
+})(angular);
